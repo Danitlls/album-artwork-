db4d2dab16ba682168329668987ac82f5e8e7de9
daniela Trulls and Karina Roush <daniela@>
2017-06-15 11:12:29 -0700

    add fadeIn for album info

--- js/scripts.js
+++ js/scripts.js
@@ -15,12 +15,114 @@
   var album7 = new Albums('The Velvet Underground & Nico', 'The Velvet Underground & Nico', '1967', '../img/velvet.jpg');
   var album8 = new Albums('Its Blitz!', 'The Yeah Yeah Yeahs', '2009', '../img/yeahs.jpg');
 
+
 // FRONT END LOGIC
   $(document).ready(function(){
     var angle = 0;
     function galleryspin(sign) {
       spinner = document.querySelector("#spinner");
-      if (!sign) { angle = angle + 45; } else { angle = angle - 45; }
+      if (!sign) {
+        angle = angle + 45;
+      } else {
+         angle = angle - 45;
+      }
       spinner.setAttribute("style","-webkit-transform: rotateY("+ angle +"deg); -moz-transform: rotateY("+ angle +"deg); transform: rotateY("+ angle +"deg);");
     };
-  });
+
+    // album info fadein/fadeout
+    $("#impala").hover(
+      function () {
+     $(".album-info").fadeIn('slow');
+     $("#album-info-album").text(album1.albumName);
+     $("#album-info-artist").text(album1.albumArtist);
+     $("#album-info-year").text(album1.albumYear);
+    },
+    function () {
+    $(".album-info").fadeOut('slow');
+    }
+    );
+
+    $("#robert").hover(
+      function () {
+     $(".album-info").fadeIn('slow');
+     $("#album-info-album").text(album2.albumName);
+     $("#album-info-artist").text(album2.albumArtist);
+     $("#album-info-year").text(album2.albumYear);
+    },
+    function () {
+    $(".album-info").fadeOut('slow');
+    }
+    );
+
+    $("#mia").hover(
+      function () {
+     $(".album-info").fadeIn('slow');
+     $("#album-info-album").text(album3.albumName);
+     $("#album-info-artist").text(album3.albumArtist);
+     $("#album-info-year").text(album3.albumYear);
+    },
+    function () {
+    $(".album-info").fadeOut('slow');
+    }
+    );
+
+    $("#miles").hover(
+      function () {
+     $(".album-info").fadeIn('slow');
+     $("#album-info-album").text(album4.albumName);
+     $("#album-info-artist").text(album4.albumArtist);
+     $("#album-info-year").text(album4.albumYear);
+    },
+    function () {
+    $(".album-info").fadeOut('slow');
+    }
+    );
+
+    $("#pink").hover(
+      function () {
+     $(".album-info").fadeIn('slow');
+     $("#album-info-album").text(album5.albumName);
+     $("#album-info-artist").text(album5.albumArtist);
+     $("#album-info-year").text(album5.albumYear);
+    },
+    function () {
+    $(".album-info").fadeOut('slow');
+    }
+    );
+
+    $("#twigs").hover(
+      function () {
+     $(".album-info").fadeIn('slow');
+     $("#album-info-album").text(album6.albumName);
+     $("#album-info-artist").text(album6.albumArtist);
+     $("#album-info-year").text(album6.albumYear);
+    },
+    function () {
+    $(".album-info").fadeOut('slow');
+    }
+    );
+
+    $("#velvet").hover(
+      function () {
+     $(".album-info").fadeIn('slow');
+     $("#album-info-album").text(album7.albumName);
+     $("#album-info-artist").text(album7.albumArtist);
+     $("#album-info-year").text(album7.albumYear);
+    },
+    function () {
+    $(".album-info").fadeOut('slow');
+    }
+    );
+
+    $("#yeahs").hover(
+      function () {
+     $(".album-info").fadeIn('slow');
+     $("#album-info-album").text(album8.albumName);
+     $("#album-info-artist").text(album8.albumArtist);
+     $("#album-info-year").text(album8.albumYear);
+    },
+    function () {
+    $(".album-info").fadeOut('slow');
+    }
+    );
+});
